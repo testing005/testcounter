609fa796fe7f22ab6b084d5fcf763476ed675902
testing005 <xzinehacker@gmail.com>
2016-12-24 14:15:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(function(req,res,next){
  res.setHeader('Access-Control-Allow-Origin','http://localhost:8080');
  res.setHeader('Access-Control-Allow-Method','GET,POST,OPTIONS,PUT,PATCH,DELETE');
  res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type');
+ res.setHeader('Access-Control-Allow-Credentials','true');
  next();
 })
 
