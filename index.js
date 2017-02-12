4dc4c185b437f534c1dfc77f26eb29e93c8afec5
testing005 <xzinehacker@gmail.com>
2017-02-12 17:54:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var io      = require('socket.io').listen(server);
 var counter=30;
 
 app.use(function(req,res,next){
- res.setHeader('Access-Control-Allow-Origin','http://localhost:8080');
+ res.setHeader('Access-Control-Allow-Origin','http://www.dotaspinz.com');
  //res.setHeader('Access-Control-Allow-Method','GET,POST,OPTIONS,PUT,PATCH,DELETE');
  res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type');
  res.setHeader('Access-Control-Allow-Credentials','true');
